5a4b0d3778d9260b6574affba311ee9b436ac6f1
aditya sadawarte <adityasadawarte01@gmail.com>
2020-06-03 17:45:40 +0530

    using stack

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,16 +1,29 @@
-; A simple boot sector that demonstrated addressing
+; A simple boot sector that demonstrates stack
 
-  [org 0x7c00]          ; Skip boot sector
+  [org 0x7c00]          ; Skip boot sector.
 
-  mov ah, 0x0e          ; scrolling teletype BIOS routine.
+  mov ah, 0x0e          ; Scrolling teletype BIOS routine.
 
-  mov al, [the_secret]
+  mov bp, 0x8000        ; Set the base of the stack a little above where BIOS is.
+  mov sp, bp            ; Loads our boot sector - so it won’t overwrite us.
+
+  push 'A'
+  push 'B'
+  push 'C'
+
+  pop bx                ; We can only pop 16-bits since we are in 16-bit boundary.
+  mov al, bl            ; Most significant bit will be added by assembler as 0x00 so ignore it.
+  int 0x10
+  
+  pop bx
+  mov al, bl
+  int 0x10
+
+  pop bx
+  mov al, bl
   int 0x10
 
   jmp $                 ; Jump to the current address forever.
-
-the_secret:
-  db 'X'
 
   times 510-($-$$) db 0 ; Tell our assembly compiler to pad out our
                         ; program with enough zero bytes (db 0) to bring us to the
