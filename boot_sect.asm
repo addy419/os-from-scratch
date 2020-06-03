e5997214bd32ea9556e833a13a5c91ae29a5f744
aditya sadawarte <adityasadawarte01@gmail.com>
2020-06-03 16:56:59 +0530

    skipping boot sector

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,21 +1,19 @@
-; A simple boot sector that prints a message to the screen using a BIOS routine.
+; A simple boot sector that demonstrated addressing
 
-mov ah, 0x0e          ; scrolling teletype BIOS routine.
+  [org 0x7c00]          ; Skip boot sector
 
-mov al, 'H'
-int 0x10              ; interrupt 0x10.
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10              ; 'l' is still on al
-mov al, 'o'
-int 0x10
+  mov ah, 0x0e          ; scrolling teletype BIOS routine.
 
-jmp $                 ; Jump to the current address forever.
+  mov al, [the_secret]
+  int 0x10
 
-times 510-($-$$) db 0 ; Tell our assembly compiler to pad out our
-                      ; program with enough zero bytes (db 0) to bring us to the
-                      ; 510 th byte.
+  jmp $                 ; Jump to the current address forever.
 
-dw 0xaa55
+the_secret:
+  db 'X'
+
+  times 510-($-$$) db 0 ; Tell our assembly compiler to pad out our
+                        ; program with enough zero bytes (db 0) to bring us to the
+                        ; 510 th byte.
+
+  dw 0xaa55
